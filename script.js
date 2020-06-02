ec8f961d9c34db1eb1a3af56b54d3291e183fa8f
dongjounkim <dongjounkim@gmail.com>
2020-06-02 16:39:02 +0200

    Créatio pop-up menu

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 "use strict";
 
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  
 
 $(document).on('click', '.arrow', function (event) {
     event.preventDefault();
