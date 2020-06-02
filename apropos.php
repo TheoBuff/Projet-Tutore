41b8be76701b1d81706491fbcee9d6b2203946a4
Théo Buffet <62326624+TheoBuff@users.noreply.github.com>
2020-06-02 13:22:14 +0200

    Oui

--- apropos.php
+++ apropos.php
@@ -16,14 +16,15 @@
 
         <div class="intro">
             <h1>Qui sommes nous?</h1>
-            <p> Agence dédiée à la promotion de l'ensemble des poursuites des études spécialisées dans le développement
-                accessible après un DUT MMi. <span> MissionDev </span> est laboriosam quis omnis odio fugit, repellendus
-                adipisci iure quibusdam. <span> Paul </span> unt commodi repudiandae, laboriosam quis omnis odio fugit,
-                quaerat dolorem reprehenderit dolores alias.<span> Michelle </span> unt commodi repudiandae, laboriosam
-                quis omnis odio fugit, quaerat dolorem reprehenderit dolores alias. <span>Théo</span> unt commodi
-                repudiandae, laboriosam quis omnis odio fugit, quaerat dolorem reprehenderit dolores alias.
-                <span>Léonard</span> unt commodi repudiandae, laboriosam quis omnis odio fugit, quaerat dolorem
-                reprehenderit dolores alias. </p>
+            <p>Agence dédiée à la promotion de l'ensemble des poursuites d'études spécialisées dans le développement
+                accessible après un DUT MMi. <span> MissionDev </span> est un projet basé sur la volonté d'aider les
+                étudiants <span> MMI
+                </span>présents et futures dans ce parcours du combattant qu'est la recherche d'études spécialement dans
+                le secteur du <span> développement </span>. Nous avons eu pour volonté de proposer la majorité des
+                études disponibles pour développeur avec une aide dans la recherche pour que chaque personne puisse
+                trouver ce qui lui correspond en évitant les différents pièges. Ce projet n'aurait jamais pu voir le
+                jour sans le travail de notre groupe composé de <span> Paul </span>, <span> Michelle </span>, <span>
+                    Léonard </span> et <span> Théo </span>. </p>
         </div>
 
         <div class="contact">
@@ -36,12 +37,13 @@
                 <p>
                     <label for="nom">Nom</label><input type="text" required name="nom">
                 </p>
+
                 <p>
                     <label for="prenom">Prénom</label><input type="text" required name="prenom">
                 </p>
 
+                <p>
                 <label for="email">Email</label><input type="email" required name="email">
-
                 </p>
 
                 <p>
@@ -64,34 +66,34 @@
     </header>
 
     <div class="cvs">
-        <div>
-        <img src="img/paul.png" alt="">
-        <h3>Paul - Chef de projet</h3>
-        <p>franchement j’adore coder c’est une passion j’adore la France, les baguettes, le fromage, et Macron </p>
-        <a href="#">Venez découvrir mon CV ici</a>
+        <div class='Paul'>
+            <img src="img/paul.png" alt="">
+            <h3>Paul - Chef de projet</h3>
+            <p>franchement j’adore coder c’est une passion j’adore la France, les baguettes, le fromage, et Macron </p>
+            <a href="#">Venez découvrir mon CV ici</a>
+            <br>
         </div>
-
-        <div class="right">
-        <img src="img/michelle.png" alt="">
-        <h3>Michelle - Développeuse</h3>
-        <p>J'aime coder, penser, resoudre des problèmes et avoir des expériences de vie!</p>
-        <a href="#">Venez découvrir mon CV ici</a>
-        <a href="#">Venez découvrir mon CV ici</a>
+        <div class='Michelle'>
+            <img src="img/michelle.png" alt="">
+            <h3>Michelle - Développeuse</h3>
+            <p>franchement j’adore coder c’est une passion j’adore la France, les baguettes, le fromage, et Macron </p>
+            <a href="#">Venez découvrir mon CV ici</a>
+            <br>
         </div>
-
-        <div>
-        <img src="img/theo.png" alt="">
-        <h3>Théo - Développeur</h3>
-        <p>franchement j’adore coder c’est une passion j’adore la France, les baguettes, le fromage, et Macron </p>
-        <a href="#">Venez découvrir mon CV ici</a>
-        <a href="#">Venez découvrir mon CV ici</a>
+        <div class='Théo'>
+            <img src="img/theo.png" alt="">
+            <h3>Théo - Développeur</h3>
+            <p>Passioné de jeux vidéos, de musique métal et du la culture Geek, je cherche à faire sans cesse de
+                nouvelles rencontres et discuter, alors si vous vous reconnaisez dans cette description venez me trouver
+                sur les réseaux</p>
+            <a href="#">Venez découvrir mon CV ici</a>
+            <br>
         </div>
-
-        <div class="right">
-        <img src="img/leonard.png" alt="">
-        <h3>Léonard - Graphiste</h3>
-        <p>franchement j’adore coder c’est une passion j’adore la France, les baguettes, le fromage, et Macron </p>
-        <a href="#">Venez découvrir mon CV ici</a>
+        <div class='Léonard'>
+            <img src="img/leonard.png" alt="">
+            <h3>Léonard - Graphiste</h3>
+            <p>franchement j’adore coder c’est une passion j’adore la France, les baguettes, le fromage, et Macron </p>
+            <a href="#">Venez découvrir mon CV ici</a>
         </div>
     </div>
 
