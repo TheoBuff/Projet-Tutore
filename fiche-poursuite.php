28943d73a73cebff3e39aee99f620213f6fb2284
Théo Buffet <62326624+TheoBuff@users.noreply.github.com>
2020-06-07 21:04:02 +0200

    last commit

--- fiche-poursuite.php
+++ fiche-poursuite.php
@@ -17,49 +17,58 @@
 $db = new PDO('mysql:host=localhost;dbname=projettut', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
-    $formation = $db->query('SELECT * FROM formation');
         ?>
 
     <div class="container">
     <?php
-       $a = $formation->fetch();
-         echo '<header><h1>'.$a['nom_etablissement'].'</h1> <p>'.$a['nom_complet'].'</p>  <div class="localisation"> <p><img src="img/icone-localisation.png" alt="localisation" width="30px" height="30px"></p>
-    </div></header> 
-    <main>
-    <div class="Savoir"><h4><span> Ce que tu dois savoir </span></h4> 
-            <p>Ce que tu as à <span> savoir </span>, est que cette formation ammène les étudiants un diplome de <span>'.$a['diplome'].'</span>. tu dois donc être bien sûr que tu veux faire tes études pour obtenir ce diplôme. Sachant que cette formation a pour principale débouché de devenir <span>'.$a['debouche_1'].'</span>. Tu peux toujours aller consulter la page de cette université directement par le <span> lien </span> plus bas si tu souhaites obtenir des informations supplémentaires</p>
-        </div>
 
-         <div class="Matières"><h4><span> Les Matières enseignées <span></h4> 
-            <ul>
-                <li><span>'.$a['matiere_1'].'</span></li>
-                <li>'.$a['matiere_2'].'</li>
-                <li><span>'.$a['matiere_3'].'</span></li>
-                <li>'.$a['matiere_4'].'</li>
-                <li><span>'.$a['matiere_5'].'</span></li>
-            </ul>
-        </div>
+if(isset($_GET["id"])){  $sql = "SELECT * FROM formation WHERE id=:id;";
+$req = $db -> prepare($sql); 
+$req -> execute(array(":id" => $_GET["id"]));
+$a = $req->fetch();  $req = null;
+echo '<header><h1>'.$a['nom_complet'].'</h1> 
+<p>'.$a['nom_etablissement'].'</p> 
+
+<div class="localisation"> <p><img src="img/icone-localisation.png" alt="localisation" 
+width="30px" height="30px"></p>
+</div></header> 
+<main>
+<div class="Savoir"><h4><span> Ce que tu dois savoir </span></h4> 
+        <p>Ce que tu as à <span> savoir </span>, est que cette formation ammène les étudiants un diplome de <span>'.$a['diplome'].'</span>. tu dois donc être bien sûr que tu veux faire tes études pour obtenir ce diplôme. Sachant que cette formation a pour principale débouché de devenir <span>'.$a['debouche_1'].'</span>. Tu peux toujours aller consulter la page de cette université directement par le <span> lien </span> plus bas si tu souhaites obtenir des informations supplémentaires</p>
+    </div>
+
+     <div class="Matières"><h4><span> Les Matières enseignées <span></h4> 
+        <ul>
+            <li><span>'.$a['matiere_1'].'</span></li>
+            <li>'.$a['matiere_2'].'</li>
+            <li><span>'.$a['matiere_3'].'</span></li>
+            <li>'.$a['matiere_4'].'</li>
+            <li><span>'.$a['matiere_5'].'</span></li>
+        </ul>
+    </div>
+
+    <div class="Débouchés"><h4><span> Les Débouchés </span></h4> 
+        <ul>
+            <li><span>'.$a['debouche_1'].'</span></li>
+            <li>'.$a['debouche_2'].'</li>
+            <li><span>'.$a['debouche_3'].'</span></li>
+        </ul> 
+    </div>
+
+    <div class="Informations"><h4><span> Les Informations essentielles </span></h4>
+        <p class="texte">'.$a['diplome'].'</p>
+        <p class="site">lien vers le site de '.$a['nom_etablissement'].' : <div class="lien-bleu"> '.$a['lien_site'].' </div></a></p>
+     </div>
+
     
-        <div class="Débouchés"><h4><span> Les Débouchés </span></h4> 
-            <ul>
-                <li><span>'.$a['debouche_1'].'</span></li>
-                <li>'.$a['debouche_2'].'</li>
-                <li><span>'.$a['debouche_3'].'</span></li>
-            </ul> 
-        </div>
+ 
+</main>';
 
-        <div class="Informations"><h4><span> Les Informations essentielles </span></h4>
-            <p class="texte">'.$a['diplome'].'</p>
-            <p class="site">lien vers le site de '.$a['nom_etablissement'].' : <div class="lien-bleu"> '.$a['lien_site'].' </div></a></p>
-         </div>
-
-        
-     
-    </main>';
+         
 
 
     
-     ?>
+ } ?>
     </div>
 
 </body>
