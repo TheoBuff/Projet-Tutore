7a73aae42f58220ce73f020e8460f68e74395455
Théo Buffet <62326624+TheoBuff@users.noreply.github.com>
2020-06-11 14:19:15 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title></title>
+</head>
+<body>
+  
+</body>
+</html>
 <?php
 //Database connection
 ini_set('display_errors', 1);
